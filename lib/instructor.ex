8ccc9e626610ffd9cfaff83b01755a8d80d42bdf
Thomas Brewer <tom.brewer@mac.com>
2024-05-29 17:33:50 -0400

    fix: added another pattern match for echo_response

--- lib/instructor.ex
+++ lib/instructor.ex
@@ -533,6 +533,21 @@ defmodule Instructor do
     ]
   end
 
+  defp echo_response(%{
+         "choices" => [
+           %{
+        "message" => %{"content" => content}
+           }
+         ]
+       }) do
+    [
+      %{
+        role: "user",
+        content: content
+      }
+    ]
+  end
+
   defp params_for_mode(mode, response_model, params) do
     json_schema = JSONSchema.from_ecto_schema(response_model)
 
